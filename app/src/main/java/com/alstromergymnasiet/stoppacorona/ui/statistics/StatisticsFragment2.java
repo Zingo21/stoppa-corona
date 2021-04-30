01a3bccc98d31aa36518dfbede5f86a1616f7de3
Zingo21 <bolengejosiane@gmail.com>
2021-04-30 13:04:05 +0200

    Add StatisticsFragment2 and started creating code

---
+++ app/src/main/java/com/alstromergymnasiet/stoppacorona/ui/statistics/StatisticsFragment2.java
@@ -0,0 +1,22 @@
+package com.alstromergymnasiet.stoppacorona.ui.statistics;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class StatisticsFragment2 extends Fragment {
+
+    ProgressBar progressBar;
+    RecyclerView rvCovidStatistics;
+    TextView tvCovidInfoFlag;
+    TextView tvCovidInfoCountry;
+    TextView tvCovidInfoCases;
+    TextView tvCovidInfoPercent;
+    TextView tvCovidInfoArrow;
+    Drawable dpArrowUp, dpArrowDown, dpArrowSide;
+
+
+}
